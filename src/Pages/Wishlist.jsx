873eecd7e4d5546ff4e24e8e43765cc292184559
akash <akashfulari007@gmail.com>
2023-04-12 10:52:45 +0530

    wishlist page comleted

--- src/Pages/Wishlist.jsx
+++ src/Pages/Wishlist.jsx
@@ -1,17 +1,35 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Button, Flex, Heading, useToast } from '@chakra-ui/react'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import CompProductCard from './Products/CompProductCard'
 import WishListCard from './WishListCard'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const Wishlist = () => {
 
   const {wishlist} = useSelector(store=>store.auth.user)
   const {user} = useSelector(store=>store.auth)
-
+  const location = useLocation()
+  const toast = useToast();
+  const navigate = useNavigate("/");
   console.log("wishlist",wishlist)
+  function goBack() {
+		window.history.back();
+	  }
+  if(wishlist.length===0){
+    return (
+      <>
+      <Flex mt={"50vh"} ml={"25vw"} >
+          <Heading position={'absolute'} textAlign={"center"}>
+            Wishlist is empty...!
+          </Heading>
+             <Button mt="5rem" onClick={()=>goBack()}>Go Back To Home </Button>
+      </Flex>
+      </>
+    )
+  }
   return (
-    <Box>
+    <Box p={"3rem"}>
 
       <Flex gap={"1rem"}>
              {
